070cc3d36edc7d6711f5ed3989b6da98b4aca163
Matthew Meyers <jedimastermeyers@gmail.com>
2020-01-16 16:01:06 -0700

    Made hex chunk not render inside faces

--- ProceduralGame/Assets/Scripts/Procedural/Hex/HexChunk.cs
+++ ProceduralGame/Assets/Scripts/Procedural/Hex/HexChunk.cs
@@ -4,71 +4,126 @@ using UnityEngine;
 
 public class HexChunk : MonoBehaviour
 {
+    public Font font;
+
     private List<Vector3> vertices = new List<Vector3>();
     private List<int> triangles = new List<int>();
     private List<Vector2> uvs = new List<Vector2>();
     public MeshFilter meshFilter => GetComponent<MeshFilter>();
 
+    public static readonly int chunkWidth = 5;
+    public static readonly int chunkHeight = 10;
+
+    List<Vector4> voxelCoords = new List<Vector4>();
+
     private void Start()
     {
-        CreateVoxelMeshData();
+        PopulateVoxelMap();
+
+        CreateChunk();
         CreateMesh();
     }
 
+    private void PopulateVoxelMap()
+    {
+        for (int y = 0; y < chunkHeight; y++)
+        {
+            for (int z = 0; z < chunkWidth; z++)
+            {
+                for (int x = 0; x < chunkWidth; x++)
+                {
+                    HexCoordinates coord = HexCoordinates.FromOffsetCoordinates(x,y,z);
+                    voxelCoords.Add(new Vector4(coord.x, coord.w, coord.z, coord.y));
+                }
+            }
+        }
+    }
+
+    private bool CheckVoxel(Vector4 pos)
+    {
+        if(voxelCoords.Contains(pos))
+            return true;
+        return false;
+    }
+
+    void CreateChunk()
+    {
+        for (int y = 0; y < chunkHeight; y++)
+        {
+            for (int z = 0; z < chunkWidth; z++)
+            {
+                for (int x = 0; x < chunkWidth; x++)
+                {
+                    float posX = (x + z * 0.5f - z / 2) * (HexVoxel.innerRadius * 2f);
+                    float posZ = z * (HexVoxel.outerRadius * 1.5f);
+                    HexCoordinates coord = HexCoordinates.FromOffsetCoordinates(x,y,z);
+                    CreateVoxelMeshData(new Vector3(posX, y, posZ), new Vector4(coord.x, coord.w,  coord.z, coord.y));
+                }
+            }
+        }
+    }
+
     //Creates the data for each voxel
-    void CreateVoxelMeshData()
+    void CreateVoxelMeshData(Vector3 pos, Vector4 checkPos)
     {
         for (int i = 0; i < HexVoxel.hexSideTris.GetLength(0); i++)
         {
-            int vertexIndex = vertices.Count;
+            if(!CheckVoxel(checkPos + HexVoxel.faceChecks[i]))
+            {
+                int vertexIndex = vertices.Count;
             
-            vertices.Add(HexVoxel.hexVerts[HexVoxel.hexSideTris[i, 0]]);
-            vertices.Add(HexVoxel.hexVerts[HexVoxel.hexSideTris[i, 1]]);
-            vertices.Add(HexVoxel.hexVerts[HexVoxel.hexSideTris[i, 2]]);
-            vertices.Add(HexVoxel.hexVerts[HexVoxel.hexSideTris[i, 3]]);
+                vertices.Add(HexVoxel.hexVerts[HexVoxel.hexSideTris[i, 0]] + pos);
+                vertices.Add(HexVoxel.hexVerts[HexVoxel.hexSideTris[i, 1]] + pos);
+                vertices.Add(HexVoxel.hexVerts[HexVoxel.hexSideTris[i, 2]] + pos);
+                vertices.Add(HexVoxel.hexVerts[HexVoxel.hexSideTris[i, 3]] + pos);
 
-            uvs.Add(HexVoxel.hexUvs[0]);
-            uvs.Add(HexVoxel.hexUvs[1]);
-            uvs.Add(HexVoxel.hexUvs[2]);
-            uvs.Add(HexVoxel.hexUvs[3]);
+                uvs.Add(HexVoxel.hexUvs[0]);
+                uvs.Add(HexVoxel.hexUvs[1]);
+                uvs.Add(HexVoxel.hexUvs[2]);
+                uvs.Add(HexVoxel.hexUvs[3]);
 
-            triangles.Add(vertexIndex);
-            triangles.Add(vertexIndex + 1);
-            triangles.Add(vertexIndex + 2);
-            triangles.Add(vertexIndex + 2);
-            triangles.Add(vertexIndex + 1);
-            triangles.Add(vertexIndex + 3);
+                triangles.Add(vertexIndex);
+                triangles.Add(vertexIndex + 1);
+                triangles.Add(vertexIndex + 2);
+                triangles.Add(vertexIndex + 2);
+                triangles.Add(vertexIndex + 1);
+                triangles.Add(vertexIndex + 3);
+            }
         }
 
         for (int i = 0; i < HexVoxel.hexTopTris.GetLength(0); i++)
         {
-            int vertexIndex = vertices.Count;
-            vertices.Add(HexVoxel.hexVerts[HexVoxel.hexTopTris[i, 0]]);
-            vertices.Add(HexVoxel.hexVerts[HexVoxel.hexTopTris[i, 1]]);
-            vertices.Add(HexVoxel.hexVerts[HexVoxel.hexTopTris[i, 2]]);
-            vertices.Add(HexVoxel.hexVerts[HexVoxel.hexTopTris[i, 3]]);
-            vertices.Add(HexVoxel.hexVerts[HexVoxel.hexTopTris[i, 4]]);
-            vertices.Add(HexVoxel.hexVerts[HexVoxel.hexTopTris[i, 5]]);
-            
-            uvs.Add(new Vector2(0f, 0.5f));
-            uvs.Add(new Vector2(0.25f, 1f));
-            uvs.Add(new Vector2(0.25f, 0f));
-            uvs.Add(new Vector2(0.75f, 1f));
-            uvs.Add(new Vector2(0.75f, 0f));
-            uvs.Add(new Vector2(1f, 0.5f));
+            if(!CheckVoxel(checkPos + HexVoxel.faceChecks[i + HexVoxel.hexSideTris.GetLength(0)]))
+            {
+                int vertexIndex = vertices.Count;
 
-            triangles.Add(vertexIndex);
-            triangles.Add(vertexIndex + 1);
-            triangles.Add(vertexIndex + 2);
-            triangles.Add(vertexIndex + 2);
-            triangles.Add(vertexIndex + 1);
-            triangles.Add(vertexIndex + 3);
-            triangles.Add(vertexIndex + 2);
-            triangles.Add(vertexIndex + 3);
-            triangles.Add(vertexIndex + 4);
-            triangles.Add(vertexIndex + 4);
-            triangles.Add(vertexIndex + 3);
-            triangles.Add(vertexIndex + 5);
+                vertices.Add(HexVoxel.hexVerts[HexVoxel.hexTopTris[i, 0]] + pos);
+                vertices.Add(HexVoxel.hexVerts[HexVoxel.hexTopTris[i, 1]] + pos);
+                vertices.Add(HexVoxel.hexVerts[HexVoxel.hexTopTris[i, 2]] + pos);
+                vertices.Add(HexVoxel.hexVerts[HexVoxel.hexTopTris[i, 3]] + pos);
+                vertices.Add(HexVoxel.hexVerts[HexVoxel.hexTopTris[i, 4]] + pos);
+                vertices.Add(HexVoxel.hexVerts[HexVoxel.hexTopTris[i, 5]] + pos);
+                
+                uvs.Add(new Vector2(0f, 0.5f));
+                uvs.Add(new Vector2(0.25f, 1f));
+                uvs.Add(new Vector2(0.25f, 0f));
+                uvs.Add(new Vector2(0.75f, 1f));
+                uvs.Add(new Vector2(0.75f, 0f));
+                uvs.Add(new Vector2(1f, 0.5f));
+
+                triangles.Add(vertexIndex);
+                triangles.Add(vertexIndex + 1);
+                triangles.Add(vertexIndex + 2);
+                triangles.Add(vertexIndex + 2);
+                triangles.Add(vertexIndex + 1);
+                triangles.Add(vertexIndex + 3);
+                triangles.Add(vertexIndex + 2);
+                triangles.Add(vertexIndex + 3);
+                triangles.Add(vertexIndex + 4);
+                triangles.Add(vertexIndex + 4);
+                triangles.Add(vertexIndex + 3);
+                triangles.Add(vertexIndex + 5);
+            }
         }
     }
 
--- ProceduralGame/Assets/Scripts/Procedural/Hex/HexVoxel.cs
+++ ProceduralGame/Assets/Scripts/Procedural/Hex/HexVoxel.cs
@@ -26,18 +26,31 @@ public static class HexVoxel
 
     public static readonly int[,] hexSideTris =
     {
-        { 1, 7, 0, 6 },
-        { 2, 8, 1, 7 },
-        { 3, 9, 2, 8 },
-        { 4, 10, 3, 9 },
-        { 5, 11, 4, 10 },
-        { 0, 6, 5, 11 }
+        { 1, 7, 0, 6 },     //top right
+        { 2, 8, 1, 7 },     //right
+        { 3, 9, 2, 8 },     //bottom right
+        { 4, 10, 3, 9 },    //bottom left
+        { 5, 11, 4, 10 },   //left
+        { 0, 6, 5, 11 }     //top left
     };
 
     public static readonly int[,] hexTopTris =
     {
         { 10, 11, 9, 6, 8, 7 }, //top face
         { 4, 3, 5, 2, 0, 1 } //bottom face
+    };
+
+    public static readonly Vector4[] faceChecks =
+    {
+        //x,w,z,y
+        new Vector4(0, -1, 1, 0),   //top right
+        new Vector4(1, -1, 0, 0),   //right
+        new Vector4(1, 0, -1, 0),   //bottom right
+        new Vector4(0, 1, -1, 0),   //bottom left
+        new Vector4(-1, 1, 0, 0),   //left
+        new Vector4(-1, 0, 1, 0),   //top left
+        new Vector4(0, 0, 0, 1),    //top
+        new Vector4(0, 0, 0, -1),   //bottom
     };
 
     public static readonly Vector2[] hexUvs =
@@ -47,4 +60,30 @@ public static class HexVoxel
         new Vector2(1.0f, 0.0f),
         new Vector2(1.0f, 1.0f) 
     };
+}
+
+[System.Serializable]
+public struct HexCoordinates
+{
+    public int x {get; private set;}
+    public int y {get; private set;}
+    public int z {get; private set;}
+    public int w {
+        get
+        {
+            return -x -z;
+        }
+    }
+
+    public HexCoordinates (int _x, int _y, int _z)
+    {
+        x = _x;
+        y = _y;
+        z = _z;
+    }
+
+    public static HexCoordinates FromOffsetCoordinates (int x, int y, int z)
+    {
+        return new HexCoordinates(x - z/2, y, z);
+    }
 }
